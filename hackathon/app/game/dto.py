c4c4e9bed6a5b14f34244c087e93dbd5d6308685
Junghoon Kim <jafarock@naver.com>
2025-05-10 19:10:13 +0900

    reorganized game status function in service to views

--- hackathon/app/game/dto.py
+++ hackathon/app/game/dto.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel
-
+from typing import List
 class GameStatusResponse(BaseModel):
     game_start_at: int
     song_length: int
+
+class GameSubmitRequest(BaseModel):
+    timestamp: List[int]
+    team: str
+
+class GameSubmitResponse(BaseModel):
+    normal: int
+    good: int
+    perfect: int
+    score: int
--- hackathon/app/game/views.py
+++ hackathon/app/game/views.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from hackathon.app.common import values
+from hackathon.app.game.dto import GameStatusResponse
+from hackathon.app.game.error import GameStartAtNotFoundError, SongLengthNotFoundError
 
 game_router = APIRouter()
 
-@game_router.get("/status")
+@game_router.get("/status", status_code=status.HTTP_200_OK)
 def get_game():
-    return {"game": "status"}
+    # game_start_at 있는지 확인
+    game_start_at = values.get('game_started_at')
+    if game_start_at is None:
+        raise GameStartAtNotFoundError()
+
+    # song_length 없으면 에러 반환환
+    song_length = values.get('song_length')
+    if song_length is None:
+        raise SongLengthNotFoundError()
+
+    # 값 반환
+    return GameStatusResponse(
+        game_start_at = game_start_at,
+        song_length = song_length
+    )
 
 @game_router.post("/submit")
 def start_game():
