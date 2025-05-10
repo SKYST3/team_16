8496b4ed6a571a0fe27dbc04ba3b91a2b30d491e
endermaru <endermaru@snu.ac.kr>
2025-05-10 20:41:37 +0900

    add test

--- hackathon/app/game/service.py
+++ hackathon/app/game/service.py
@@ -1,7 +1,12 @@
 import random
 
-def test()->int:
+def test(lst: list[int]) -> int:
+    """
+    This function takes a list of integers and returns the sum of the integers in the list.
+    """
     sum = 0
     for i in range(100):
         sum += random.randint(1, 100)
     return sum
+
+    return sum(lst)
