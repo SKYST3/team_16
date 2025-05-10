747034a4aaf1af4041a654e53601c2b2183ac190
Junghoon Kim <jafarock@naver.com>
2025-05-10 20:31:13 +0900

    wrote the basic code for game-submit api

--- hackathon/app/game/dto.py
+++ hackathon/app/game/dto.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import List
+
 class GameStatusResponse(BaseModel):
     game_start_at: int
     song_length: int
--- hackathon/app/game/views.py
+++ hackathon/app/game/views.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status
 from hackathon.app.common import values
-from hackathon.app.game.dto import GameStatusResponse
+from hackathon.app.game.dto import GameStatusResponse, GameSubmitResponse, GameSubmitRequest
 from hackathon.app.game.error import GameStartAtNotFoundError, SongLengthNotFoundError
 
 game_router = APIRouter()
@@ -23,6 +23,24 @@ def get_game():
         song_length = song_length
     )
 
-@game_router.post("/submit")
-def start_game():
-    return {"message": "Game started"}
+@game_router.post("/submit", response = GameSubmitResponse)
+def submit_score(submission: GameSubmitRequest):
+    answer_timestamp = values.get('beat_list')
+
+    normal_count = 0
+    good_count = 0
+    perfect_count = 0
+    total_score = 0
+
+    submitted_timestamps = submission.timestamp
+
+    # Need function to extract closest submitted_timestamp to answer key
+
+    # Function that calculates the perfect, good, misses
+
+    return GameSubmitResponse(
+        normal=normal_count,
+        good=good_count,
+        perfect=perfect_count,
+        score=total_score
+    )
